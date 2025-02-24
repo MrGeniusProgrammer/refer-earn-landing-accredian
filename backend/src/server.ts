f953aeab87405eb9e103eb003addb8fa632562d3
MrGeniusProgrammer <mrgeniusprogrammer69@gmail.com>
2025-02-24 20:51:07 +0300

    formatted and initial setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,34 +1,18 @@
-import express, { Express, Request, Response } from "express";
 import cors from "cors";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import express, { Request, Response } from "express";
 
-dotenv.config();
-
-const app: Express = express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const uri: string =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/your-app";
-
-(async () => {
-  try {
-    await mongoose.connect(uri);
-    console.log("Connected to the database");
-  } catch (error) {
-    console.error(error);
-  }
-})();
-
 app.get("/health", (_req: Request, res: Response) => {
-  res.status(200).send("Server is running");
+	res.status(200).send("Server is running");
 });
 
-const PORT: string | number = process.env.PORT || 3000;
+const PORT: string | number = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on PORT: ${PORT}`);
+	console.log(`Server is running on PORT: ${PORT}`);
 });
